6f8b7d36c0992b9dfee2d9cdf5e8cef68e3011ee
cagilan <425905@cognizant.com>
2019-10-06 22:19:19 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+java-rest-service-example
+=========================
+
+Java Rest-Ful Services Sample using Jersey
+
+The sample program has two methods
+1. Sample restful method! Usage of annotations.
+2. How to invoke a rest method. Client request and response.
---
+++ src/main/java/org/rm/restful/manager/impl/ApplicationConstants.java
@@ -0,0 +1,6 @@
+package org.rm.restful.manager.impl;
+
+public class ApplicationConstants {
+	public static final String CHANGE_RECORDS_LIST = "tempChangeLog/getModifiedRecords";
+	public static final String SERVER_URL_KEY = "http://localhost:8081/testServer/services/";
+}
